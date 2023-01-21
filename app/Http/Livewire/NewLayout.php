0c260a84f272f97441b17d20e661ec66fc4d8d39
akram-tello <a.tello@graduate.utm.my>
2023-01-22 02:04:29 +0800

    add livewire

---
+++ app/Http/Livewire/NewLayout.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class NewLayout extends Component
+{
+    public function render()
+    {
+        return view('livewire.new-layout');
+    }
+}
